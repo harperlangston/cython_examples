f55d0f42045a94deb5da659f3b4ea79810b6e064
Harper Langston <langston@fundy.reservoir.com>
2019-03-06 15:47:03 -0500

    changes to main for sort test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyswap
+import numpy as np
 
 x = 3
 y = 4
@@ -8,3 +9,8 @@ print([x,y])
 x, y = pyswap.py_swap_int(x,y)
 
 print([x,y])
+
+values = np.array([4, 3, 5, 100, 1])
+
+pyswap.py_sort_int(values, 5)
+
